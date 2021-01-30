f488531b822021ee87a955f2c85d49c8610fd3f4
unknown <mark.nicoll0@gmail.com>
2021-01-30 13:29:41 +0100

    log statement for debugging

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -3,6 +3,7 @@ import pool from '../dbPool.js';
 export const getCategories = (req, res) => {
     pool.connect(function (err) {
         pool.query('SELECT * FROM public."categories"', function (err, result) {
+            console.log(result)
             if (err) res.send(err);
             if (result) res.send(result.rows);
         });
