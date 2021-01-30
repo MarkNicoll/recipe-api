08063d239347ed4ee2312c37637263a92b3831a8
unknown <mark.nicoll0@gmail.com>
2021-01-30 15:36:54 +0100

    Adds CORS

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import router from './routes.js';
 
 dotenv.config();
 const app = express()
+const cors = require('cors');
 
-app.use('/recipeapi/', router)
+
+app.use(cors);
+app.use('/recipeapi/', router);
 
 app.listen(process.env.PORT || 'localhost', 3000, function (err) {
     console.log(process.env.PORT)
