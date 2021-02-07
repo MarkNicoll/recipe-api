4949aac58f01130d56af01deda5fbfcd6c583b29
Mark Nicoll <markinicoll@gmail.com>
2021-02-07 16:54:27 +0000

    Updated POST request

--- src/controllers/dishesController.js
+++ src/controllers/dishesController.js
@@ -8,11 +8,12 @@ export const getDishes = (req, res) => {
 };
 
 export const createDish = (req, res) => {
-  const name = req.body.name;
-  const notes = req.body.notes;
+  const id = req.body.dish.id ? req.body.dish.id : null  
+  const name = req.body.dish.name;
+  const notes = req.body.dish.notes;
 
     client.query(
-      "REPLACE INTO dishes(name, notes)VALUES('" + name + "','" + notes + "')",
+      "REPLACE INTO dishes(id, name, notes)VALUES('" + id + "','" + name + "','" + notes + "')",
       (err, result) => {
         if (err) res.send(err.toString());
         if (result) res.send(result.rows);
