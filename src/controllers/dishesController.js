8430e8ad1e79044d5b123cc9119c575d5bd49fa4
Mark Nicoll <markinicoll@gmail.com>
2021-02-07 18:50:05 +0000

    Debugging

--- src/controllers/dishesController.js
+++ src/controllers/dishesController.js
@@ -8,11 +8,8 @@ export const getDishes = (req, res) => {
 };
 
 export const createDish = (req, res) => {
-console.log(req.body)
   const dish = req.body;
-  console.log(dish)
-
-  const id = dish.id ? req.body.dish.id : null  
+  const id = dish.id ? dish.id : null  
   const name = dish.name;
   const notes =dish.notes;
 
